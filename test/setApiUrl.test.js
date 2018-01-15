621ac6d4d6b3f81e1caf769d645b074520efed3f
Kai Mallea <kmallea@gmail.com>
2018-01-15 04:20:11 -0500

    Port tests to Jest: setApiUrl

---
+++ test/setApiUrl.test.js
@@ -0,0 +1,30 @@
+const imgur = require('../lib/imgur');
+
+beforeEach(() => {
+    const defaultImgurAPIUrl = 'https://api.imgur.com/3/';
+    imgur.setAPIUrl(defaultImgurAPIUrl);
+});
+
+test('should return the API Url that was set', () => {
+    const imgurAPIUrl = 'https://imgur-apiv3.p.mashape.com/';
+    imgur.setAPIUrl(imgurAPIUrl);
+    expect(imgur.getAPIUrl()).toBe(imgurAPIUrl);
+});
+
+test('should not set an empty API Url', () => {
+    const imgurAPIUrl = '';
+    imgur.setAPIUrl(imgurAPIUrl);
+    expect(imgur.getAPIUrl()).not.toBe(imgurAPIUrl);
+});
+
+test('should not set a number', () => {
+    const imgurAPIUrl = 1024;
+    imgur.setAPIUrl(imgurAPIUrl);
+    expect(imgur.getAPIUrl()).not.toBe(imgurAPIUrl);
+});
+
+test('should not set a boolean', () => {
+    const imgurAPIUrl = false;
+    imgur.setAPIUrl(imgurAPIUrl);
+    expect(imgur.getAPIUrl()).not.toBe(imgurAPIUrl);
+});
