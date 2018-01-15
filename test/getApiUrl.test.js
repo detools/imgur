9103acbdd1d5ee4861e96f74028c97613a3886e7
Kai Mallea <kmallea@gmail.com>
2018-01-15 04:06:19 -0500

    Remove unnecessary calls from test

--- test/getApiUrl.test.js
+++ test/getApiUrl.test.js
@@ -2,15 +2,11 @@ const imgur = require('../lib/imgur');
 
 test('should return the default API URL, if nothing is set', () => {
     const defaultApiUrl = 'https://api.imgur.com/3/';
-
-    expect.assertions(1);
     expect(imgur.getAPIUrl()).toBe(defaultApiUrl);
 });
 
 test('should return the same API URL that was set', () => {
     const apiUrl = 'https://imgur-apiv3.p.mashape.com/';
     imgur.setAPIUrl(apiUrl);
-    
-    expect.assertions(1);
     expect(imgur.getAPIUrl()).toBe(apiUrl);
 });
